132c756890b0f0c3a284e1d1815ee4244f7e24a2
sat <77387934+sixsat@users.noreply.github.com>
2025-06-21 18:13:22 +0700

    add read and write file

---
+++ functions/get_file_content.py
@@ -0,0 +1,23 @@
+import os
+
+
+def get_file_content(working_directory, file_path):
+    abs_working_dir = os.path.abspath(working_directory)
+    target_file = os.path.abspath(os.path.join(working_directory, file_path))
+    if not target_file.startswith(abs_working_dir):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(target_file):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+
+    MAX_CHARS = 10000
+    try:
+        with open(target_file, "r") as f:
+            content = f.read(MAX_CHARS)
+            if len(content) == MAX_CHARS:
+                content = (
+                    content[:MAX_CHARS]
+                    + f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                )
+        return content
+    except Exception as e:
+        return f'Error reading file "{file_path}": {e}'
