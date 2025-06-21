fa2bddd11ab8495f3f61cc0fc4e8a854d35411dd
sat <77387934+sixsat@users.noreply.github.com>
2025-06-21 22:20:01 +0700

    add function schema and update agent tool

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,4 +1,7 @@
 import os
+from google.genai import types
+
+MAX_CHARS = 10000
 
 
 def get_file_content(working_directory, file_path):
@@ -9,7 +12,6 @@ def get_file_content(working_directory, file_path):
     if not os.path.isfile(target_file):
         return f'Error: File not found or is not a regular file: "{file_path}"'
 
-    MAX_CHARS = 10000
     try:
         with open(target_file, "r") as f:
             content = f.read(MAX_CHARS)
@@ -21,3 +23,19 @@ def get_file_content(working_directory, file_path):
         return content
     except Exception as e:
         return f'Error reading file "{file_path}": {e}'
+
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description=f"Reads and returns the first {MAX_CHARS} characters of the content from a specified file within the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file whose content should be read, relative to the working directory.",
+            ),
+        },
+        required=["file_path"],
+    ),
+)
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from call_function import available_functions
+
 
 def main():
     load_dotenv()
@@ -27,15 +29,34 @@ def main():
 
 
 def generate_content(client, messages, isVerbose):
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+    - Read file contents
+    - Execute Python files with optional arguments
+    - Write or overwrite files
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
     response = client.models.generate_content(
         # https://ai.google.dev/gemini-api/docs/models
         model="gemini-2.0-flash-001",
         contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
     )
-    print(response.text)
     if isVerbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
+    if not response.function_calls:
+        print(response.text)
+        return
+    for func in response.function_calls:
+        print(f"Calling function: {func.name}({func.args})")
 
 
 if __name__ == "__main__":
